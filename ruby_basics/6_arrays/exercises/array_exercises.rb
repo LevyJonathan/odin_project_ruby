3540c1748d459fd039851694bbb98d6df310e576
levy <levi.viana.bezerra@gmail.com>
2023-08-14 18:06:22 -0300

    adicionando os exercicios 6_arrays

--- ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -1,7 +1,6 @@
 def nil_array(number)
   # return an array containing `nil` the given number of times
-  num_array[]
-  num_array.new(number)
+  Array.new(number)
 end
 
 def first_element(array)
@@ -21,34 +20,37 @@ end
 
 def add_element(array)
   # add an element (of any value) to the array
-  array.push(array)
-  array
+  # array_teste = []
+  # array_teste.push(array)
+  Array.new.push(array)
 end
 
 def remove_last_element(array)
   # Step 1: remove the last element from the array
-  array.pop
+  array_teste = array
+  p array_teste.pop
   # Step 2: return the array (because Step 1 returns the value of the element removed)
-  array
+  array_teste
 end
 
 def remove_first_three_elements(array)
   # Step 1: remove the first three elements
-  array.unshift
+  array_teste = array
+  array_teste.shift(3)
   # Step 2: return the array (because Step 1 returns the values of the elements removed)
-  array
+  array_teste
 end
 
 def array_concatenation(original, additional)
   # return an array adding the original and additional array together
-  original + additional
+  # original + additional
   # original << additional
-  # original.concat(additional)
+  original.concat(additional)
 end
 
 def array_difference(original, comparison)
   # return an array of elements from the original array that are not in the comparison array
-  original - additional
+  original - comparison
 end
 
 def empty_array?(array)
@@ -68,19 +70,28 @@ end
 
 def include?(array, value)
   # return true if the array includes the value
-  array.push(value)
+  array_teste = array
+  array_teste.push(value)
   array.include?(value)
 end
 
 def join(array, separator)
   # return the result of joining the array with the separator
-  array.join('-')
+  array.join(separator)
 end
 
 # calling functions
-nil_array(4)
-first_element('teste')
-third_element('teste')
-last_three_elements('teste')
-add_element('teste2')
-remove_last_element("teste")
+p nil_array(4)
+puts first_element('teste')
+puts third_element('teste')
+puts last_three_elements(['teste1','teste2','teste3','teste4','teste5'])
+puts add_element('teste2')
+p remove_last_element(['teste1','teste2','teste3','teste4','teste5'])
+p remove_first_three_elements(['teste1','teste2','teste3','teste4','teste5'])
+p array_concatenation(['Olá'], ['Mundo'])
+p array_difference(['futebol', 'volei', 'natacao'], ['volei', 'rugby', 'futebol'])
+puts empty_array?([])
+p reverse(['l', 'e', 'v', 'y'])
+puts array_length(['l', 'e', 'v', 'y'])
+p include?(['olá'], 'mundo')
+p join(['snake', 'case'], '_')
